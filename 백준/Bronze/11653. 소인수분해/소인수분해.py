9a64ae333f6ebec966a1f8f8a1bbb001d04bc632
4회차_이수정 <119299742+Shureeshu@users.noreply.github.com>
2023-01-18 00:53:08 +0900

    [Bronze I] Title: 소인수분해, Time: 44 ms, Memory: 30748 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,12 +1,19 @@
 def factorize(n):
-    i = 2
-    while n > 1 :
-        while i <= n:
-            if n%i == 0:
-                print(i)
-                n //= i
-            else:
-                i += 1
+    # print('factorize', n)
+    lst = []
 
-N = int(input())
-factorize(N)
+    i = int(n**(1/2))
+    while n > 1 and i > 1:
+        if i > 1 and n%i == 0:
+            # print(i, n//i)
+            lst += factorize(i) 
+            lst += factorize(n//i)
+            return lst
+        i -= 1
+    if n == 1:
+        return []
+    return [n]
+
+N = int(input()) # (1 ≤ N ≤ 10,000,000)
+rst = sorted(factorize(N))
+[print(p) for p in rst]
