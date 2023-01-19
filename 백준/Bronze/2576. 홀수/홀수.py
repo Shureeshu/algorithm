a848fe22550d03410703a03f1bb0ce7c6e5d5282
4회차_이수정 <119299742+Shureeshu@users.noreply.github.com>
2023-01-19 14:06:07 +0900

    [Bronze III] Title: 홀수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.py
+++ 백준/Bronze/2576. 홀수/홀수.py
@@ -1,16 +1,10 @@
 N = 7
-n = []
-for i in range(N):
-    pass
-    n.append(int(input()))
-n = tuple(n)
-
 odd_n = []
-for x in n:
-    if x%2:
-        odd_n.append(x)
-odd_n = tuple(odd_n)
-
+for i in range(N): # 입력의 첫째 줄부터 일곱 번째 줄까지
+    # 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100보다 작다.
+    n = int(input())
+    if n%2:
+        odd_n.append(n)
 if len(odd_n):
     print(sum(odd_n))
     print(min(odd_n))
