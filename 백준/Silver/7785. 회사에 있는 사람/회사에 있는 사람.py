146c93c22c2ffae9f69d433ecd53a9f8df1a15c4
4회차_이수정 <119299742+Shureeshu@users.noreply.github.com>
2023-01-19 16:00:12 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 4068 ms, Memory: 48792 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -4,5 +4,5 @@ for i in range(1, N+1):
     name, detail = input().split()
     members[name] = members.get(name, 0) + 1
 
-current = sorted([name for name in members if members[name]%2], reverse=True)
-[print(name) for name in current]
+current = sorted(members.items(), reverse=True)
+[print(name[0]) for name in current if name[1]%2]
