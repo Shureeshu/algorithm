91abab8461a3a9fdafa4d2d7102c521342c68d0f
4회차_이수정 <119299742+Shureeshu@users.noreply.github.com>
2023-01-22 23:02:14 +0900

    [Silver IV] Title: 알고리즘 수업 - 병합 정렬 1, Time: 316 ms, Memory: 89532 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
@@ -1,52 +1,18 @@
 import sys
-def merge(A, p, q, r, c):
-    i, j, t = p, q+1, 0
-    tmp = A[p:r+1]
-    while(i <= q and j <=r):
-        if A[i]<=A[j]:
-            tmp[t] = A[i]
-            i += 1
-            t += 1
-        else:
-            tmp[t] = A[j]
-            j += 1
-            t += 1
-    while (i<=q):
-        tmp[t] = A[i]
-        i += 1
-        t += 1
-    while (j <= r):
-        tmp[t] = A[j]
-        j += 1
-        t += 1
-    i, t = p, 0
-
-    while i <= r:
-        A[i] = tmp[t]
-        c += 1
-        if c == k:
-
-            print(A[i])
-            exit()
-        i += 1
-        t += 1
-
-    return c
-
-def merge_sort(A, p, r, c):
+def ms_c(p, r, k, c):
     if p < r:
         q = (p+r)//2
-        c = merge_sort(A, p, q, c)
+        c = ms_c(p, q, k, c)
+        c = ms_c(q+1, r, k, c)
+        c += r-p+1
+        if c >= k:
+            print(sorted(A[p:r+1])[-1-c+k])
+            exit()
+    return c
 
-        c = merge_sort(A, q+1, r, c)
-
-        c = merge(A, p, q, r, c)
-
-        return c
-    else:
-        return c
 
 N, k = map(int, input().split())
 A = list(map(int, sys.stdin.readline().split()))
-merge_sort(A, 0, N-1, 0)
+ms_c(0,N-1,k,0)
+# merge_sort(A, 0, N-1, 0)
 print(-1)
