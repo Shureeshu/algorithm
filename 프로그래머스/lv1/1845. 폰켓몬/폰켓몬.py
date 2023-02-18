544cb99c9e4bb65e77f15a3a08bb05373fb75915
4회차_이수정 <119299742+Shureeshu@users.noreply.github.com>
2023-02-19 00:04:40 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,4 +1,5 @@
 def solution(nums):
-    N = len(nums)
-    answer = min(N//2, len(set(nums)))
+    num_choose = len(nums)//2
+    num_kinds = len(set(nums))
+    answer = min(num_choose, num_kinds)
     return answer
