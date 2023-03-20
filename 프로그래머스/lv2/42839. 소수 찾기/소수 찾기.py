3954ccf2a5776b04e684589ef842eaac2275d595
4회차_이수정 <119299742+Shureeshu@users.noreply.github.com>
2023-03-20 10:57:06 +0900

    [level 2] Title: 소수 찾기, Time: 0.52 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -6,13 +6,13 @@ def solution(numbers):
     arr = arr.difference([-1])
     return len(arr)
 
-def isPrime(x):
+def isPrime(x): # O(x^(1/2))
     x = int(x)
     if x in [0, 1, 4, 6, 8, 9]:
         return -1
     if x in [2, 3, 5, 7]:
         return x
-    for i in range(2, x):
+    for i in range(2, int(x**(1/2)+1)):
         if not x%i:
             return -1
     return x
